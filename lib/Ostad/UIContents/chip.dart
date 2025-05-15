df676c2bfc074e9f9fdd8a765a0361bacab884bf
md. ruhul amin <m.rasarker77@gmail.com>
2025-05-15 16:18:55 +0600

    wizard separation add chip

---
+++ lib/Ostad/UIContents/chip.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+class chip extends StatelessWidget {
+  const chip({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Size screenSize = MediaQuery.of(context).size;
+    // double screenWidth = MediaQuery.of(context).size.width;
+    double fontSize = screenSize.width > 600 ? 32 : 18;
+    double pexelRatio = MediaQuery.of(context).devicePixelRatio;
+    return  Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Container(
+              width: screenSize.width*0.5,
+              height: screenSize.height*0.2,
+              color: Colors.red,
+            ),
+          ),
+
+          Text('This is Media Query text',
+            style: TextStyle(
+                fontSize: fontSize
+            ),
+          ),
+          Text('pixel Ratio : $pexelRatio'),
+          Text('Wrap and List container wizard use'),
+          Wrap(
+            spacing: 8.0,
+            runSpacing: 4.0,
+            children: List.generate(10, (index) {
+              return Container(
+                color: Colors.red,
+                height: 50,
+                width: 50,
+              );
+            }),
+
+          ),
+          SizedBox(height: 5,),
+          Text('Wrap and List with Chip wizard use'),
+          Wrap(
+            spacing: 8.0,
+            runSpacing: 4.0,
+            children: List.generate(10, (index) {
+
+              return Chip(label: Text('Chip $index',style: TextStyle(color: Colors.red),),
+
+              );
+            }),
+
+          ),
+
+
+
+          //
+          // LayoutBuilder(builder: (context,constraints){
+          //   if(constraints.maxWidth > 600){
+          //     return GridView.count(crossAxisCount: 5);
+          //   }else{
+          //     return GridView.count(crossAxisCount: 1);
+          //   }
+          // })
+        ],
+      ),
+
+    );
+  }
+}
